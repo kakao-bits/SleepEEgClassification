38822f7e261c53222cb268080251c1263a47671a
alex <934685522@qq.com>
2018-12-07 14:31:48 +0800

    修改load文件路径 Signed-off-by: alex <934685522@qq.com>

--- ucd_slp_feature_classification/code/ucd_testfeature_coarse_grain.m
+++ ucd_slp_feature_classification/code/ucd_testfeature_coarse_grain.m
@@ -1,8 +1,8 @@
 function [deep_count,mix_count,test_N_deep,test_N_mix] = ucd_testfeature_coarse_grain(u,SVMModel,deep_count,mix_count,test_N_deep,test_N_mix)
 %%  测试ucd数据集的特征
-name_deep = sprintf('ucd%03d_deepf6_feature',u);
-name_mix = sprintf('ucd%03d_mixf6_feature',u);
-name_wake = sprintf('ucd%03d_wakef6_feature',u);
+name_deep = sprintf('data/ucd%03d_deepf6_feature',u);
+name_mix = sprintf('data/ucd%03d_mixf6_feature',u);
+name_wake = sprintf('data/ucd%03d_wakef6_feature',u);
 
 load(name_deep)
 load(name_mix)
